79c59e9fe02f55a04e03d850ca3d67c5b06acfbb
Josh Pinkney <joshpinkney@gmail.com>
2020-10-30 13:09:28 -0400

    a
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import * as core from '@actions/core'
-import * as path from 'path'
-import { validateInputs } from './validate'
-import { copyMetaYAML, getLastestMetaYAMLVersion, getNextVersion } from './create';
+import { getLastestMetaYAMLVersion } from './create';
 import { updateLatestTxt, updateVSCodeExtensionsJSON } from './update';
 import { createNewMetaYAML, writeNewMetaYAML } from './modify';
+import * as github from '@actions/github';
 
 /**
  * Main function that is ran when the github action is started
@@ -11,12 +10,38 @@ import { createNewMetaYAML, writeNewMetaYAML } from './modify';
 async function run(): Promise<void> {
   try {
 
-    const inputs = validateInputs();
+    const myToken = core.getInput('myToken');
 
-    const next = getNextVersion();
-    process.chdir(path.join('che-plugin-registry', 'v3', 'plugins', inputs.pluginPath))
+    const octokit = github.getOctokit(myToken)
+    
+    const extensionsJSON = require('./vscode-extensions.json')
+    for (const ext of extensionsJSON.extensions) {
+      // Check the repository to see if it has a newer tag
+      const repository = ext.repository;
+      const tags = octokit.repos.listTags({
+        owner: "a",
+        repo: repository
+      });
+      /**
+       * [
+          {
+            "name": "v0.1",
+            "commit": {
+              "sha": "c5b97d5ae6c19d5c5df71a34c7fbeeda2479ccbc",
+              "url": "https://api.github.com/repos/octocat/Hello-World/commits/c5b97d5ae6c19d5c5df71a34c7fbeeda2479ccbc"
+            },
+            "zipball_url": "https://github.com/octocat/Hello-World/zipball/v0.1",
+            "tarball_url": "https://github.com/octocat/Hello-World/tarball/v0.1"
+          }
+        ]
+       */
+      // test to see if the revision matches name
+      // if it fails then test to see if the revision matches commit.sha
+    }
+
+
+
     const latest = getLastestMetaYAMLVersion()
-    copyMetaYAML(latest, next)
     const newMetadata = createNewMetaYAML(latest, next)
     writeNewMetaYAML(newMetadata)
     updateLatestTxt(next)
